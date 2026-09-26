333c9afbc92f4df464abd8e725eca26b1514c638
Seifsaad <seifsaad2018@gmail.com>
2026-09-26 17:15:46 +0300

    5th commit

--- src/common/email/nodemailer.js
+++ src/common/email/nodemailer.js
@@ -1,18 +1,16 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth:{
-        user:process.env.USER_EMAIL,
-        pass:process.env.USER_PASS
-    }
-
-});
-
 export async function sendEmail(to,subject,html) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth:{
+            user:process.env.USER_EMAIL,
+            pass:process.env.USER_PASS
+        }
 
+    });
     await transporter.sendMail({
         from: ` "whisper-app" <${process.env.USER_EMAIL}>`,
         to:to,
--- src/main.js
+++ src/main.js
@@ -13,6 +13,19 @@ app.use('/auth', authRouter);
 app.use('/message', messageRouter);
 app.use('/user', userRouter);
 
+app.use((err, req, res, next)=>{
+    console.log(err)
+    if(err.isOperational === true) {
+        return  res.status(err.statusCode).json({
+            message: err.message,
+            success: false
+        })
+    }
+    return res.status(500).json({
+        error: 'something went wrong',
+        success: false
+    })
+})
 
 app.listen(3000,()=>{
     console.log('Server started on port 3000!');
